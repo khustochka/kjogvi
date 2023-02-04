c16f3ee17da5b92f71acafc71b090791d57e0d12
Vitalii Khustochka <xyctka@gmail.com>
2023-02-04 13:25:51 -0600

    Fix the function and add specs.

--- apps/ornithologue/lib/ornitho/query/book.ex
+++ apps/ornithologue/lib/ornitho/query/book.ex
@@ -5,13 +5,15 @@ defmodule Ornitho.Query.Book do
 
   import Ecto.Query
 
+  @spec by_signature(Ecto.Queryable.t(), String, String) :: Ecto.Query.t()
   def by_signature(query, slug, version) do
     from [..., book: b] in query,
     where: b.slug == ^slug and b.version == ^version
   end
 
-  def by_signature(query, slug, version) do
-    from [..., book: b] in query,
-    where: b.slug == ^slug and b.version == ^version
+  @spec by_signature(Ecto.Queryable.t(), %{:slug => String, :version => String, optional(any) => any}) ::
+          Ecto.Query.t()
+  def by_signature(query, %{slug: slug, version: version}) do
+    by_signature(query, slug, version)
   end
 end
