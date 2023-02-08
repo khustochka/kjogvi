510891ccaf87447a05e5bf7f9e9c92824651b4cc
Vitalii Khustochka <xyctka@gmail.com>
2023-02-07 21:54:16 -0600

    Import book from prepared CSV.
    CSV is prepared in a separate script.

--- apps/ornithologue/lib/ornitho/importer/ebird.v2022.ex
+++ apps/ornithologue/lib/ornitho/importer/ebird.v2022.ex
@@ -3,18 +3,7 @@ defmodule Ornitho.Importer.Ebird.V2022 do
   Importer for eBird/Clements checklist version 2022.
   """
 
-  @extras_conversion %{
-    "TAXON_ORDER" => {:integer, :ebird_order},
-    "SPECIES_GROUP" => {:string, :species_group},
-    "Clements v2022 change" => {:string, :change_type},
-    "text for website v2022" => {:string, :change_text},
-    "range" => {:string, :range},
-    "extinct" => {:boolean, :extinct},
-    "extinct year" => {:string, :extinct_year}
-  }
-
-  @ebird_taxonomy_file "priv/import/ebird/v2022/ebird_taxonomy_v2022.csv"
-  @clements_checklist_file "priv/import/ebird/v2022/NEW_Clements-Checklist-v2022-October-2022.csv"
+  @prepared_taxonomy_file "priv/import/ebird/v2022/ornithologue_ebird_v2022.csv"
 
   use Ornitho.Importer,
     slug: "ebird",
@@ -27,106 +16,31 @@ defmodule Ornitho.Importer.Ebird.V2022 do
         "Downloaded from https://www.birds.cornell.edu/clementschecklist/download/"
 
   def create_taxa(book) do
-    create_taxa_from_csv(book, @ebird_taxonomy_file)
-    amend_taxa_from_csv(book, @clements_checklist_file)
+    create_taxa_from_csv(book, @prepared_taxonomy_file)
   end
 
   def create_taxa_from_csv(book, csv_file) do
     csv_file
     |> File.stream!([:trim_bom])
     |> CSV.decode(headers: true)
-    |> Enum.reduce({1, %{}}, fn {:ok, row}, {sort_order, species_cache} ->
-      family =
-        case row["FAMILY"] do
-          "" -> nil
-          str -> Regex.run(~r/\A\w+dae/, str) |> List.first()
-        end
+    |> Enum.reduce(%{}, fn {:ok, row}, taxa_cache ->
 
-      attrs = %{
-        name_sci: row["SCI_NAME"],
-        name_en: row["PRIMARY_COM_NAME"],
-        code: row["SPECIES_CODE"],
-        category: row["CATEGORY"],
-        order: row["ORDER1"],
-        family: family,
-        extras: extract_extras(row),
-        sort_order: sort_order,
-        parent_species_id: species_cache[row["REPORT_AS"]]
-      }
+      {:ok, extras} = Jason.decode(row["extras"])
+      attrs =
+        row
+        |> Map.put("parent_species_id", taxa_cache[row["parent_species_code"]])
+        |> Map.put("extras", extras)
 
       {:ok, taxon} = Ornitho.create_taxon(book, attrs)
 
       new_cache =
-        if row["CATEGORY"] == "species" do
-          Map.put(species_cache, row["SPECIES_CODE"], taxon.id)
+        if row["category"] == "species" do
+          Map.put(taxa_cache, row["code"], taxon.id)
         else
-          species_cache
+          taxa_cache
         end
 
-      {sort_order + 1, new_cache}
+      new_cache
     end)
-  end
-
-  defp amend_taxa_from_csv(book, csv_file) do
-    csv_file
-    |> File.stream!([:trim_bom])
-    |> CSV.decode(headers: true)
-    |> Enum.each(fn {:ok, row} ->
-      taxon = Ornitho.Find.Taxon.by_name_sci(book, row["scientific name"])
-
-      if not is_nil(taxon) do
-        new_extras = taxon.extras |> Map.merge(extract_extras(row))
-        {authority, authority_brackets} = extract_authority(row["authority"])
-
-        Ornitho.update_taxon(
-          taxon,
-          %{extras: new_extras, authority: authority, authority_brackets: authority_brackets}
-        )
-      end
-    end)
-  end
-
-  defp extract_extras(row) do
-    Map.keys(@extras_conversion)
-    |> Enum.reduce(%{}, fn key, acc ->
-      {type, field} = @extras_conversion[key]
-
-      add_converted_key(type, row[key], field)
-      |> Map.merge(acc)
-    end)
-  end
-
-  defp add_converted_key(:string, value, field) do
-    with str when is_binary(str) and str != "" <- value do
-      %{field => str}
-    else
-      _ -> %{}
-    end
-  end
-
-  defp add_converted_key(:integer, value, field) do
-    with str when is_binary(str) and str != "" <- value do
-      %{field => String.to_integer(str)}
-    else
-      _ -> %{}
-    end
-  end
-
-  defp add_converted_key(:boolean, value, field) do
-    with "1" <- value do
-      %{field => true}
-    else
-      _ -> %{}
-    end
-  end
-
-  # {authority_string, has_brackets?}
-  defp extract_authority("" = _authority), do: {nil, nil}
-
-  defp extract_authority(authority) do
-    {
-      Regex.named_captures(~r/\A\(?(?<authority>[^\)]*)\)?\Z/, authority)["authority"],
-      String.starts_with?(authority, "(")
-    }
   end
 end
--- apps/ornithologue/mix.exs
+++ apps/ornithologue/mix.exs
@@ -27,6 +27,7 @@ defmodule Ornithologue.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "scripts"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
