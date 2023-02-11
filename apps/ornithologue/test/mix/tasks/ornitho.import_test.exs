d05af01e0d42aae8f2836e9c761efe2e4248f0ca
Vitalii Khustochka <xyctka@gmail.com>
2023-02-11 11:14:13 -0600

    Fix the successful mix import test.

--- apps/ornithologue/test/mix/tasks/ornitho.import_test.exs
+++ apps/ornithologue/test/mix/tasks/ornitho.import_test.exs
@@ -1,9 +1,10 @@
 defmodule Mix.Tasks.Ornitho.ImportTest do
   use ExUnit.Case
+  use Ornitho.RepoCase, async: true
 
   alias Ornitho.Importer
 
-  test "when non-existent module is passed it fails" do
+  test "when non-existent module is given it fails" do
     assert_raise Mix.Error,
                  "Could not load Importer module Ornitho.Importer.Fake.V2, error: :nofile.",
                  fn ->
@@ -11,7 +12,7 @@ defmodule Mix.Tasks.Ornitho.ImportTest do
                  end
   end
 
-  test "when module without `process_import` is passed it fails" do
+  test "when module without `process_import` is given it fails" do
     assert_raise Mix.Error,
                  "Module Mix.Tasks.Ornitho.ImportTest is not an Importer, " <>
                    "needs to define function 'process_import/1'.",
@@ -20,8 +21,7 @@ defmodule Mix.Tasks.Ornitho.ImportTest do
                  end
   end
 
-  # @tag skip: "Causes Ecto error"
-  # test "passes with correct importer" do
-  #   Mix.Task.rerun("ornitho.import", [Importer.Demo.V1])
-  # end
+  test "passes with correct importer" do
+    Mix.Task.rerun("ornitho.import", [Importer.Demo.V1])
+  end
 end
