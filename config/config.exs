201530b68fa6a4d0c93ae769337c8ec497b3d4ff
Vitalii Khustochka <xyctka@gmail.com>
2023-02-10 20:13:10 -0600

    Update tailwind. Underline links.

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :kjogvi_web, KjogviWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.41",
+  version: "0.17.7",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.4",
+  version: "3.2.6",
   default: [
     args: ~w(
       --config=tailwind.config.js
