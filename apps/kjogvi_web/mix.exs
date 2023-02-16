c8f8b4852e549237562544d5438b80e910acfaad
Vitalii Khustochka <xyctka@gmail.com>
2023-02-15 21:56:56 -0600

    Upgrade deps.

--- apps/kjogvi_web/mix.exs
+++ apps/kjogvi_web/mix.exs
@@ -37,7 +37,7 @@ defmodule KjogviWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.2", override: true},
+      {:phoenix, "~> 1.7.0-rc.3", override: true},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
