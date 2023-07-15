5aaf38fde9ad47dfcf84c41521e0c035f1e6aeb3
Vitalii Khustochka <xyctka@gmail.com>
2023-07-15 10:51:22 -0500

    Taxon expansion logic (no UI).

--- apps/kjogvi_web/lib/kjogvi_web/live/taxa_live/table.ex
+++ apps/kjogvi_web/lib/kjogvi_web/live/taxa_live/table.ex
@@ -3,9 +3,26 @@ defmodule KjogviWeb.TaxaLive.Table do
 
   import KjogviWeb.TaxaComponents
 
-  attr :skip_parent_species, :boolean, default: false
+  @impl true
+  def handle_event("expand_taxon", %{"code" => code}, socket) do
+    {:noreply,
+      socket
+      |> assign(:expanded_taxon, code)
+    }
+  end
+
+  @impl true
+  def handle_event("collapse_taxon", _params, socket) do
+    {:noreply,
+      socket
+      |> assign(:expanded_taxon, nil)
+    }
+  end
+
   attr :book, Ornitho.Schema.Book, required: true
   attr :taxa, :list, required: true
+  attr :skip_parent_species, :boolean, default: false
+  attr :expanded_taxon, :string, default: nil
 
   def render(assigns) do
     ~H"""
@@ -39,6 +56,14 @@ defmodule KjogviWeb.TaxaLive.Table do
         <:col :let={taxon} label="taxonomy">
             <div><%= taxon.order %></div>
             <div><%= taxon.family %></div>
+        </:col>
+        <:col :let={taxon} label="">
+            <div :if={taxon.code != @expanded_taxon}>
+              <.link phx-click="expand_taxon" phx-target={@myself} phx-value-code={taxon.code}>Expand</.link>
+            </div>
+            <div :if={taxon.code == @expanded_taxon}>
+              <.link phx-click="collapse_taxon" phx-target={@myself}>Collapse</.link>
+            </div>
         </:col>
       </.simpler_table>
     </div>
