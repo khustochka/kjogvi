c3e8f532a4601dab445a582637ef1a735bbb6ec1
Vitalii Khustochka <xyctka@gmail.com>
2023-07-15 10:09:27 -0500

    Extract module attributes for like sanitization.

--- apps/ornithologue/lib/ornitho/query/utils.ex
+++ apps/ornithologue/lib/ornitho/query/utils.ex
@@ -1,5 +1,8 @@
 defmodule Ornitho.Query.Utils do
+  @like_special_symbols ~r/(%|_|\\)/
+  @like_replace_pattern "\\\\\\1"
+
   def sanitize_like(str) do
-    String.replace(str, ~r/(%|_|\\)/, "\\\\\\1")
+    String.replace(str, @like_special_symbols, @like_replace_pattern)
   end
 end
