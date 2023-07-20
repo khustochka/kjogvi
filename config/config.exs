6300175b87c5787be5735ea1014be75907a1145b
Vitalii Khustochka <xyctka@gmail.com>
2023-07-19 23:10:03 -0500

    Upgrades.

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :kjogvi_web, KjogviWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.18.11",
+  version: "0.18.14",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.3.2",
+  version: "3.3.3",
   default: [
     args: ~w(
       --config=tailwind.config.js
