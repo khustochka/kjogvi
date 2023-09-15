1912b80c8388a637d5dc2619f85f5b8f4f30d0ba
Vitalii Khustochka <xyctka@gmail.com>
2023-09-15 17:58:04 -0500

    Dependencies.

--- apps/ornitho_web/config/config.exs
+++ apps/ornitho_web/config/config.exs
@@ -8,7 +8,7 @@ config :logger, :console, format: "[$level] $message\n"
 
 if config_env() == :dev do
   config :esbuild,
-    version: "0.19.1",
+    version: "0.19.3",
     default: [
       args:
         ~w(js/app.js --bundle --minify --target=es2017 --outdir=../dist/js --external:/fonts/* --external:/images/*),
--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,7 @@ config :kjogvi_web, KjogviWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.19.1",
+  version: "0.19.3",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
