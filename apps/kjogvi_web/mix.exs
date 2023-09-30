4a495ee917178568414a0331760b3c6c57a9eab9
Vitalii Khustochka <xyctka@gmail.com>
2023-09-29 19:46:02 -0500

    Upgrade dependencies.

--- apps/kjogvi_web/mix.exs
+++ apps/kjogvi_web/mix.exs
@@ -45,7 +45,7 @@ defmodule KjogviWeb.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.19.0"},
+      {:phoenix_live_view, "~> 0.20.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
