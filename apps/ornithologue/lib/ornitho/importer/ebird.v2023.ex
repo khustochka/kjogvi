3f7bd717b56bfc244aa2cbe5a25669f483154e6a
Vitalii Khustochka <xyctka@gmail.com>
2023-11-24 19:24:26 -0600

    Format.

--- apps/ornithologue/lib/ornitho/importer/ebird.v2023.ex
+++ apps/ornithologue/lib/ornitho/importer/ebird.v2023.ex
@@ -9,8 +9,8 @@ defmodule Ornitho.Importer.Ebird.V2023 do
     name: "eBird/Clements Checklist v2023",
     description:
       "Clements, J. F., P. C. Rasmussen, T. S. Schulenberg, M. J. Iliff, T. A. Fredericks, " <>
-      "J. A. Gerbracht, D. Lepage, A. Spencer, S. M. Billerman, B. L. Sullivan, " <>
-      "and C. L. Wood. 2023. The eBird/Clements checklist of Birds of the World: v2023.\n" <>
+        "J. A. Gerbracht, D. Lepage, A. Spencer, S. M. Billerman, B. L. Sullivan, " <>
+        "and C. L. Wood. 2023. The eBird/Clements checklist of Birds of the World: v2023.\n" <>
         "Downloaded from https://www.birds.cornell.edu/clementschecklist/download/"
 
   @prepared_taxonomy_file "priv/import/ebird/v2023/ornithologue_ebird_v2023.csv"
