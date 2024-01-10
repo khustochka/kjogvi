2b0ee569ecead1d942e458d7546bea33484e51b8
Vitalii Khustochka <xyctka@gmail.com>
2024-01-09 22:02:11 -0600

    Consolidate Ornitho migrations.

--- apps/ornithologue/priv/repo/migrations/v01.ex
+++ apps/ornithologue/priv/repo/migrations/v01.ex
@@ -1,12 +1,29 @@
-defmodule Ornitho.Repo.Migrations.AddTaxaTable do
+defmodule Ornitho.Repo.Migrations.V01 do
   use Ecto.Migration
 
   def change do
+    # BOOKS
+    create table(:books) do
+      add :slug, :string, null: false, size: 16
+      add :version, :string, null: false, size: 16
+      add :name, :string, null: false, size: 256
+      add :description, :text
+      add :extras, :map
+      add :imported_at, :utc_datetime_usec
+
+      timestamps()
+    end
+
+    create index(:books, [:slug, :version], unique: true)
+
+    # TAXA
+
     create table(:taxa) do
       add :book_id, references("books", on_delete: :delete_all), null: false
       add :name_sci, :string, size: 256, null: false
       add :name_en, :string
       add :code, :string, size: 256, null: false
+      add :taxon_concept_id, :string, size: 256
       add :category, :string, size: 32
       add :authority, :string
       add :authority_brackets, :boolean
@@ -26,6 +43,7 @@ defmodule Ornitho.Repo.Migrations.AddTaxaTable do
     create index(:taxa, [:book_id, :name_sci], unique: true)
     create index(:taxa, [:book_id, :code], unique: true)
     create index(:taxa, [:book_id, :sort_order], unique: true)
+    create index(:taxa, [:book_id, :taxon_concept_id], unique: true)
 
     create index(:taxa, [:parent_species_id],
              unique: false,
