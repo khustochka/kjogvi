927f6c0b331b98fb592a1a5dec416e40bca5b1d3
Vitalii Khustochka <xyctka@gmail.com>
2024-01-19 17:45:16 -0600

    Add os_mon.

--- apps/kjogvi_web/mix.exs
+++ apps/kjogvi_web/mix.exs
@@ -30,7 +30,7 @@ defmodule KjogviWeb.MixProject do
   def application do
     [
       mod: {KjogviWeb.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
