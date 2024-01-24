39457754bc767e37a24b15fc8c192796b9557651
Vitalii Khustochka <xyctka@gmail.com>
2024-01-23 22:09:16 -0600

    Phoenix libs not a hard dependency for ornitho_web.

--- apps/ornitho_web/mix.exs
+++ apps/ornitho_web/mix.exs
@@ -44,10 +44,10 @@ defmodule OrnithoWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.7"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix, "~> 1.7.7", only: [:dev, :test]},
+      {:phoenix_html, "~> 3.3", only: [:dev, :test]},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.0"},
+      {:phoenix_live_view, "~> 0.20.0", only: [:dev, :test]},
       {:floki, ">= 0.30.0", only: :test},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
