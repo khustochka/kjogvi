91455b928d41a8e970cc56c9f67a42b8df24a247
Vitalii Khustochka <xyctka@gmail.com>
2024-02-06 22:02:02 -0600

    Async legacy import.

--- apps/kjogvi_web/lib/kjogvi_web/live/admin/tasks/index.ex
+++ apps/kjogvi_web/lib/kjogvi_web/live/admin/tasks/index.ex
@@ -1,20 +1,56 @@
 defmodule KjogviWeb.Live.Admin.Tasks.Index do
   use KjogviWeb, :live_view
 
+  alias Phoenix.LiveView.AsyncResult
+
   def mount(_params, _session, socket) do
     {
       :ok,
       socket
       |> assign(:page_title, "Admin Tasks")
+      |> assign(:async_result, %AsyncResult{})
     }
   end
 
   def handle_event("legacy_import", _params, socket) do
-    Kjogvi.Legacy.Import.run()
-
     {:noreply,
      socket
-     |> put_flash(:info, "Legacy import processed.")}
+     |> start_legacy_import()
+     |> clear_flash()}
+  end
+
+  defp start_legacy_import(socket) do
+    # live_view_pid = self()
+
+    socket
+    |> assign(:async_result, AsyncResult.loading())
+    |> start_async(:legacy_import, fn ->
+      Kjogvi.Legacy.Import.run()
+      # Enum.each(1..5, fn n ->
+      #   Process.sleep(1_000)
+      #   IO.puts("SENDING ASYNC TASK MESSAGE #{n}")
+      #   send(live_view_pid, {:task_message, "Async work chunk #{n}"})
+      # end)
+      :ok
+    end)
+  end
+
+  def handle_async(:legacy_import, {:ok, :ok = _success_result}, socket) do
+    socket =
+      socket
+      |> put_flash(:info, "Legacy import done.")
+      |> assign(:async_result, AsyncResult.ok(%AsyncResult{}, :ok))
+
+    {:noreply, socket}
+  end
+
+  def handle_async(:legacy_import, {:exit, reason}, socket) do
+    socket =
+      socket
+      |> put_flash(:error, "Legacy import failed: #{inspect(reason)}")
+      |> assign(:async_result, %AsyncResult{})
+
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -23,7 +59,11 @@ defmodule KjogviWeb.Live.Admin.Tasks.Index do
     <h2>Legacy Import</h2>
     <.simple_form for={nil} phx-submit="legacy_import" action={~p"/admin/tasks/legacy_import"}>
       <:actions>
-        <.button phx-disable-with="processing...">Import</.button>
+        <%= if @async_result.loading do %>
+          <.button disabled>processing...</.button>
+        <% else %>
+          <.button>Import</.button>
+        <% end %>
       </:actions>
     </.simple_form>
     """
