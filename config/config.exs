d38ff2f7d112cdb73e87c93b64bfa8cbe28855b3
Vitalii Khustochka <xyctka@gmail.com>
2024-03-01 19:59:23 -0600

    Upgrade deps.

--- config/config.exs
+++ config/config.exs
@@ -44,7 +44,7 @@ config :kjogvi_web, KjogviWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.20.0",
+  version: "0.20.1",
   kjogvi_web: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
