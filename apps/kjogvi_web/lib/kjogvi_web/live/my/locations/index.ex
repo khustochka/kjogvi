6d646cd948a89c4cb51f702550a72001fac75afe
Vitalii Khustochka <xyctka@gmail.com>
2024-10-17 22:09:12 -0500

    Simple locations accordion.

--- apps/kjogvi_web/lib/kjogvi_web/live/my/locations/index.ex
+++ apps/kjogvi_web/lib/kjogvi_web/live/my/locations/index.ex
@@ -68,12 +68,16 @@ defmodule KjogviWeb.Live.My.Locations.Index do
     <ul>
       <%= for location <- @locations do %>
         <li>
-          <div class="flex gap-2">
+          <details open class="[&_svg]:open:-rotate-90">
+          <summary class="flex gap-2">
+            <svg class="rotate-0 transform text-blue-700 transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
             <div><%= location.id %></div>
             <div><%= location.slug %></div>
             <div><%= location.name_en %></div>
             <div><%= location.cards_count %></div>
-          </div>
+          </summary>
           <div class="ml-8">
             <%= render_with_children(%{
               locations:
@@ -81,6 +85,7 @@ defmodule KjogviWeb.Live.My.Locations.Index do
               all_locations: @all_locations
             }) %>
           </div>
+          </details>
         </li>
       <% end %>
     </ul>
