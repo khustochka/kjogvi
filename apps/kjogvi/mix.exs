17061ef21d393c74d4c2b9aa20acc707deb6ef2a
Vitalii Khustochka <xyctka@gmail.com>
2024-10-18 20:28:12 -0500

    Deps update.

--- apps/kjogvi/mix.exs
+++ apps/kjogvi/mix.exs
@@ -60,7 +60,7 @@ defmodule Kjogvi.MixProject do
       {:ecto_sql, "~> 3.12"},
       {:ecto_psql_extras, "~> 0.8"},
       {:postgrex, ">= 0.0.0"},
-      {:cachex, "~> 3.6"},
+      {:cachex, "~> 4.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.17.0"},
       {:finch, "~> 0.13"},
--- apps/ornitho_web/config/config.exs
+++ apps/ornitho_web/config/config.exs
@@ -8,7 +8,7 @@ config :logger, :console, format: "[$level] $message\n"
 
 if config_env() == :dev do
   config :esbuild,
-    version: "0.23.0",
+    version: "0.24.0",
     ornitho_web: [
       args:
         ~w(js/app.js --bundle --minify --target=es2017 --outdir=../dist/js --external:/fonts/* --external:/images/*),
@@ -18,7 +18,7 @@ if config_env() == :dev do
 
   # Configure tailwind (the version is required)
   config :tailwind,
-    version: "3.4.6",
+    version: "3.4.14",
     ornitho_web: [
       args: ~w(
         --config=tailwind.config.js
