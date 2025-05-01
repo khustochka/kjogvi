af5861ddb21ea8de367493fc4bc19eef114a70e7
Vitalii Khustochka <xyctka@gmail.com>
2025-05-01 00:32:40 -0500

    Simplify DD instrumentation.

--- apps/kjogvi/lib/kjogvi/opentelemetry/ecto.ex
+++ apps/kjogvi/lib/kjogvi/opentelemetry/ecto.ex
@@ -3,19 +3,22 @@ defmodule Kjogvi.Opentelemetry.Ecto do
   Enables Ecto opentelemetry with customizations specific to the Kjogvi project.
   """
 
+  @default_opts [db_statement: :enable]
+
   def setup() do
     Application.fetch_env!(:kjogvi, :ecto_repos)
     |> Enum.each(fn repo ->
       repo_key = split_repo_key(repo)
-      OpentelemetryEcto.setup(repo_key, default_opts(repo_key))
+      OpentelemetryEcto.setup(repo_key, @default_opts)
     end)
   end
 
-  defp default_opts(repo_key) do
-    # Overwriting service name like this will only work in Datadog.
-    # Other OTEL services just treat this as a regular tag and use the global service name.
-    [db_statement: :enabled, additional_attributes: %{"service.name": service_name(repo_key)}]
-  end
+  # defp default_opts(repo_key) do
+  #   # Overwriting service name like this will only work in Datadog.
+  #   # Other OTEL services just treat this as a regular tag and use the global service name.
+  #   # NOTE: this is not needed anymore, since Datadog detects DB name.
+  #   [db_statement: :enabled, additional_attributes: %{"service.name": service_name(repo_key)}]
+  # end
 
   defp split_repo_key(repo) do
     repo
@@ -27,9 +30,9 @@ defmodule Kjogvi.Opentelemetry.Ecto do
     end)
   end
 
-  defp service_name(repo_key) do
-    repo_key |> Enum.map_join("-", &Atom.to_string/1)
-  end
+  # defp service_name(repo_key) do
+  #   repo_key |> Enum.map_join("-", &Atom.to_string/1)
+  # end
 
   # defp service_addon([_ | rest]) do
   #   rest |> Enum.map(&Atom.to_string/1) |> Enum.join("-")
