dd6295ee3edaa4238d3fa8ffc401a3c27c00e0f7
Vitalii Khustochka <xyctka@gmail.com>
2025-11-08 15:09:28 -0600

    Format.

--- apps/ornithologue/scripts/convertor/avilist.v2025.exs
+++ apps/ornithologue/scripts/convertor/avilist.v2025.exs
@@ -32,83 +32,84 @@ defmodule Convertor.AviList.V2025 do
       csv_file
       |> File.stream!([:trim_bom])
       |> CSV.decode(headers: true)
-      |> Enum.reduce({1, {nil, nil}, []}, fn {:ok, row},
-                                             {sort_order,
-                                              {last_species_code, last_species_name_en}, taxa_acc} ->
-        taxon_rank = row["Taxon_rank"]
+      |> Enum.reduce(
+        {1, {nil, nil}, []},
+        fn {:ok, row}, {sort_order, {last_species_code, last_species_name_en}, taxa_acc} ->
+          taxon_rank = row["Taxon_rank"]
 
-        if taxon_rank in ["species", "subspecies"] do
-          code = row["Sequence"] |> String.pad_leading(5, "0")
+          if taxon_rank in ["species", "subspecies"] do
+            code = row["Sequence"] |> String.pad_leading(5, "0")
 
-          {authority, authority_brackets} = extract_authority(row["Authority"])
+            {authority, authority_brackets} = extract_authority(row["Authority"])
 
-          parent_species_code =
-            if taxon_rank == "species" do
-              nil
-            else
-              last_species_code
-            end
-
-          name_en_raw = row["English_name_AviList"]
-
-          name_en =
-            cond do
-              is_binary(name_en_raw) and name_en_raw != "" ->
-                name_en_raw
-
-              taxon_rank == "subspecies" and not is_nil(last_species_name_en) ->
-                subspecies_epithet =
-                  row["Scientific_name"]
-                  |> String.split(" ")
-                  |> List.last()
-
-                "#{last_species_name_en} (#{subspecies_epithet})"
-
-              :else ->
-                raise "Invalid state"
-            end
-
-          {:ok, encoded_extras} =
-            extract_extras(row)
-            |> then(fn extras ->
-              if extras[:extinction_status] in ["Extinct", "(extinct)"] do
-                Map.put(extras, :extinct, true)
+            parent_species_code =
+              if taxon_rank == "species" do
+                nil
               else
-                extras
+                last_species_code
               end
-            end)
-            |> Jason.encode()
 
-          attrs =
-            [
-              code: code,
-              sort_order: sort_order,
-              category: taxon_rank,
-              name_sci: row["Scientific_name"],
-              name_en: name_en,
-              order: row["Order"],
-              family: row["Family"],
-              authority: authority,
-              authority_brackets: authority_brackets,
-              taxon_concept_id: row["AvibaseID"],
-              protonym: row["Protonym"],
-              extras: encoded_extras,
-              parent_species_code: parent_species_code
-            ]
+            name_en_raw = row["English_name_AviList"]
 
-          new_last_species_tuple =
-            case taxon_rank do
-              "species" -> {code, name_en}
-              "subspecies" -> {last_species_code, last_species_name_en}
-            end
+            name_en =
+              cond do
+                is_binary(name_en_raw) and name_en_raw != "" ->
+                  name_en_raw
 
-          new_taxa_list = [attrs | taxa_acc]
+                taxon_rank == "subspecies" and not is_nil(last_species_name_en) ->
+                  subspecies_epithet =
+                    row["Scientific_name"]
+                    |> String.split(" ")
+                    |> List.last()
 
-          {sort_order + 1, new_last_species_tuple, new_taxa_list}
-        else
-          {sort_order, {nil, nil}, taxa_acc}
+                  "#{last_species_name_en} (#{subspecies_epithet})"
+
+                :else ->
+                  raise "Invalid state"
+              end
+
+            {:ok, encoded_extras} =
+              extract_extras(row)
+              |> then(fn extras ->
+                if extras[:extinction_status] in ["Extinct", "(extinct)"] do
+                  Map.put(extras, :extinct, true)
+                else
+                  extras
+                end
+              end)
+              |> Jason.encode()
+
+            attrs =
+              [
+                code: code,
+                sort_order: sort_order,
+                category: taxon_rank,
+                name_sci: row["Scientific_name"],
+                name_en: name_en,
+                order: row["Order"],
+                family: row["Family"],
+                authority: authority,
+                authority_brackets: authority_brackets,
+                taxon_concept_id: row["AvibaseID"],
+                protonym: row["Protonym"],
+                extras: encoded_extras,
+                parent_species_code: parent_species_code
+              ]
+
+            new_last_species_tuple =
+              case taxon_rank do
+                "species" -> {code, name_en}
+                "subspecies" -> {last_species_code, last_species_name_en}
+              end
+
+            new_taxa_list = [attrs | taxa_acc]
+
+            {sort_order + 1, new_last_species_tuple, new_taxa_list}
+          else
+            {sort_order, {nil, nil}, taxa_acc}
+          end
         end
-      end)
+      )
 
     Enum.reverse(taxa)
   end
