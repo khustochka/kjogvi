ca8802340c68af9e6855705b5bec67e4e3af6e61
Vitalii Khustochka <xyctka@gmail.com>
2025-11-28 23:19:50 -0600

    Regex backwards compatibility.

--- config/dev.exs
+++ config/dev.exs
@@ -80,12 +80,12 @@ config :kjogvi_web, SampleAppWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/kjogvi_web/router\.ex$"E,
-      ~r"lib/kjogvi_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/kjogvi_web/router\.ex$",
+      ~r"lib/kjogvi_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
