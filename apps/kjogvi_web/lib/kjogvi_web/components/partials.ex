5d44e0959621f52ebb146fe89f2ca5a20115cdbf
Vitalii Khustochka <xyctka@gmail.com>
2026-06-14 21:11:43 -0500

    Add rudimentary user menu.

--- apps/kjogvi_web/lib/kjogvi_web/components/partials.ex
+++ apps/kjogvi_web/lib/kjogvi_web/components/partials.ex
@@ -7,9 +7,18 @@ defmodule KjogviWeb.Partials do
 
   import KjogviWeb.AdminMenuComponents
 
+  alias Kjogvi.Accounts.User
+
   require Kjogvi.Config
 
   embed_templates "partials/*"
+
+  @doc """
+  Sub-menu shown in the public chrome when browsing a specific user's section,
+  linking to that user's own lifelist and photos.
+  """
+  attr :scope, Kjogvi.Scope, required: true
+  def subject_user_menu(assigns)
 
   attr :list, :list, doc: "List of observations", required: true
   attr :total, :integer, doc: "Total number of species", required: true
---
+++ apps/kjogvi_web/test/kjogvi_web/components/subject_user_menu_test.exs
@@ -0,0 +1,29 @@
+defmodule KjogviWeb.SubjectUserMenuTest do
+  use KjogviWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  import Kjogvi.AccountsFixtures
+
+  test "shows the subject user's lifelist and photos links in the user section", %{conn: conn} do
+    user = user_fixture(display_name: "Jane Birder")
+
+    {:ok, live, _html} = live(conn, ~p"/users/#{user.nickname}")
+
+    assert has_element?(live, "#subject-user-menu")
+
+    assert has_element?(
+             live,
+             "#subject-user-menu a[href='/users/#{user.nickname}']",
+             "Jane Birder"
+           )
+
+    assert has_element?(live, "#subject-user-menu a[href='/users/#{user.nickname}/lifelist']")
+    assert has_element?(live, "#subject-user-menu a[href='/users/#{user.nickname}/photos']")
+  end
+
+  test "is absent in the community section", %{conn: conn} do
+    {:ok, live, _html} = live(conn, ~p"/community/lifelist")
+
+    refute has_element?(live, "#subject-user-menu")
+  end
+end
