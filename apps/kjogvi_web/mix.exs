cff8849e83eebe8049e492f27c001eaf3d96a26d
Vitalii Khustochka <xyctka@gmail.com>
2026-08-18 20:38:09 -0500

    Update deps.

--- apps/kjogvi_web/mix.exs
+++ apps/kjogvi_web/mix.exs
@@ -62,7 +62,7 @@ defmodule KjogviWeb.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.2.0"},
-      {:phoenix_live_dashboard, "~> 0.8.4"},
+      {:phoenix_live_dashboard, "~> 0.9.0"},
       {:oban_web, "~> 2.12"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.5.0", runtime: Mix.env() == :dev},
